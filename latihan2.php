9f770f0e8e05fc5facc8568f786458664f53b794
dendy iman lesmana <107296412+dendyimanuelcesar@users.noreply.github.com>
2023-04-23 20:27:36 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,39 @@
+<?php 
+require 'function.php';
+$menu = query("SELECT * FROM pertemuan 7");
+?>
+
+
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiwa</title>
+</head>
+<body>
+  <h3>Daftar Mahasiwa</h3>
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+
+    <?php $i = 1;
+    foreach($menu as $m) : ?>
+    <tr>
+      <td>1</td>
+      <td><?= $m['nrp']; ?></td>
+      <td><?= $m['nama']; ?></td>
+      <td><?= $m['email']; ?></td>
+      <td><?= $m['jurusan']; ?></td>
+    </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+</html>
---
+++ pertemuan_7.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 23 Apr 2023 pada 14.45
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pertemuan 7`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `nim` int(20) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `jurusan` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
